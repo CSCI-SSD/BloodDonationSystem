fcb8cec5999b49fd991103ec31c82d094d6cae59
CSCI-SSD <kpuchakayala@unomaha.edu>
2015-07-30 19:12:09 -0500

    Initial Commit

---
+++ BloodDonationSystem/src/com/bds/beans/LoginBean.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package com.bds.beans;
+
+/**
+ * @author kartheek
+ * 
+ * @
+ *
+ */
+public class LoginBean {
+	
+	private String userName;
+	
+	private String password;
+	
+	private String status;
+	
+	private String type;
+	
+	private String lastName;
+	
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+}
